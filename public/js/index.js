9a8a5a37c1fc127879fd8cff5687ffe5d9777cb6
m.sawastian <m.sawastian@gmail.com>
2018-08-07 11:42:51 +0200

    Emit newMessage event on createMessage event.

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server!')
-
-    socket.emit('createMessage', {
-        from: 'example@example.com',
-        text: 'Hello!'
-    })
 });
 
 socket.on('disconnect', function() {
