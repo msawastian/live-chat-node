2241458b6b33629836e0465ae64c650abc07c0a3
m.sawastian <m.sawastian@gmail.com>
2018-08-07 16:31:49 +0200

    Basic frontend for displaying and sending messages.

--- public/js/index.js
+++ public/js/index.js
@@ -9,6 +9,27 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-    console.log(message)
+    console.log(message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messagesList').append(li);
 });
 
+socket.emit('createMessage', {
+    from: 'Mat',
+    text: 'Hey!'
+}, function(data) {
+    console.log(data)
+});
+
+jQuery('#inputForm').on('submit', function (event) {
+    event.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'Olga',
+        text: jQuery('[name=message]').val()
+    }, function () {
+
+    })
+});
